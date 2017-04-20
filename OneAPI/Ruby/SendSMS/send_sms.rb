3110720f8170590b62681c0c44cbf3972b97519d
Dave Baddeley <dave.baddeley@comapi.com>
2017-04-20 12:20:23 +0100

    Ruby Send SMS examle code

---
+++ OneAPI/Ruby/SendSMS/send_sms.rb
@@ -0,0 +1,50 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+# Your Comapi settings
+ApiSpace = "***ADD YOUR API SPACE ID HERE***"
+Token = "***ADD YOUR SECURITY TOKEN HERE***"
+
+puts ""
+puts "Sending SMS using Comapi and Ruby"
+puts "---------------------------------"
+
+# Create the RESTful URL
+url = URI("https://api.comapi.com/apispaces/" + ApiSpace + "/messages")
+
+# Setup the conneciton object
+http = Net::HTTP.new(url.host, url.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+# Setup the HTTP request
+request = Net::HTTP::Post.new(url)
+request["authorization"] = 'Bearer ' + Token
+request["content-type"] = 'application/json'
+request["cache-control"] = 'no-cache'
+
+# Create the Comapi request JSON
+request.body = 
+    "{
+      \"body\": \"Your SMS message\",
+      \"to\": {
+          \"phoneNumber\": \"447123123123\"
+        },
+      \"channelOptions\": {
+          \"sms\": {
+              \"from\": \"Comapi\",
+              \"allowUnicode\": true
+            }
+        },
+     \"rules\": [ \"sms\" ]
+     }"
+
+# Call the web service
+puts ""
+puts "Calling Comapi..."
+response = http.request(request)
+
+puts ""
+puts "Call returned status code: " + response.code
+puts response.read_body
