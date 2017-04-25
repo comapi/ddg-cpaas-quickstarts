c0b5100c7e126bb26bde79d631dda04b98d1819b
Dave Baddeley <dave.baddeley@comapi.com>
2017-04-25 12:46:19 +0100

    Improved the NodeJS send SMS code

--- OneAPI/Node/SendSMS/send_with_options.js
+++ OneAPI/Node/SendSMS/send_with_options.js
@@ -12,6 +12,10 @@ var yourComapiAccessToken = 'YOUR_ACCESS_TOKEN';
 // Enter your mobile number in international format here e.g. for the UK 447123123123
 var yourMobileNumber = 'YOUR_MOBILE_NUMBER';
 
+console.log('');
+console.log('Sending SMS using Comapi and NodeJS');
+console.log('-----------------------------------');
+
 // Create Comapi RESTful URL with API Space Id in it
 var comapiUrl = "https://api.comapi.com/apispaces/" + yourComapiAPISpaceId + "/messages";
 
@@ -43,8 +47,25 @@ var options = {
 };
 
 // Send the request
+console.log('');
+console.log('Calling Comapi...');
+
 request(options, function (error, response, body) {
     if (error) throw new Error(error);
-    console.log('SMS messgae successfully sent via Comapi "One" API');
+
+    console.log("HTTP status code returned: " + response.statusCode);
+
+    // Check status
+    if (response.statusCode == 201)
+    {
+        // All ok
+        console.log('SMS message successfully sent via Comapi "One" API');
+    }
+    else
+    {
+        // Something went wrong
+        console.log('Something went wrong!');
+    }
+
     console.log(body);
 });
