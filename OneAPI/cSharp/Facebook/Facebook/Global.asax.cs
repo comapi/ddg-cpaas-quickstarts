6adc2cef3efc8387f88da476a909595b7dd315e2
Dave Baddeley <DaveBaddeley@users.noreply.github.com>
2018-10-09 09:46:59 +0100

    Updated to use TLS 1.1 or 1.2

--- OneAPI/cSharp/Facebook/Facebook/Global.asax.cs
+++ OneAPI/cSharp/Facebook/Facebook/Global.asax.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Net;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -12,6 +9,9 @@ namespace Facebook
     {
         protected void Application_Start()
         {
+            // Ensure we use later versions of TLS for security
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072 | (SecurityProtocolType)768;
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
